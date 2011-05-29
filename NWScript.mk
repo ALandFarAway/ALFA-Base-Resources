9816ebe8ec167beb27bcfa8e65e2ce96c4fd8db2
Basilica <basilica@danifae.net>
2011-05-29 12:38:17 -0700

    Fix escape characters causing beeps in nwscript compilation due to nmake handling of escape strings

--- NWScript.mk
+++ NWScript.mk
@@ -6,12 +6,12 @@
 NSS_FLAGS=$(NSS_FLAGS) -x "error " -q -e -v1.69 -i $(NSS_INC_PATH) -i $(NSS_INCLUDES);
 
 {}.nss{$(OUTPUTDIR)}.ncs:
-	$(BUILD_MSG) (NWScript) Compiling - $<
+	$(BUILD_MSG) (NWScript) Compiling - $(<:.\=)
 	$(NSS_COMPILER) $(NSS_FLAGS) $< $*.ncs
 	@copy /Y $< $*.nss >NUL
 
 {..\}.nss{$(OUTPUTDIR)}.ncs:
-	$(BUILD_MSG) (NWScript) Compiling - $<
+	$(BUILD_MSG) (NWScript) Compiling - $(<:.\=)
 	@$(NSS_COMPILER) $(NSS_FLAGS) $< $*.ncs
 	@copy /Y $< $*.nss >NUL
 
