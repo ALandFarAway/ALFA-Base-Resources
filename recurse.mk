37150fc20bd1feff901a38529f326e334132ed51
Basilica <basilica@danifae.net>
2011-06-03 21:11:20 -0700

    Build system improvements; convert makefiles to use wildcards.
    - Makefiles now generally use wildcards.  ACR can't because it has
      scripts to compile.  HAKs without any generated content can be
      built via wildcards.
    
    - Make the build system more robust against release directory not
      existing.
    
    - Make the build system more robust if a directory fails to build.
      Now we'll try the rest of the directories after showing an error
      and print a list of directories that had problems at the end of
      the build.

--- recurse.mk
+++ recurse.mk
@@ -4,20 +4,40 @@
 #
 
 all: $(DIRS)
-	@for %a in ($**) do @(                             \
-	$(BUILD_MSG) [BUILD] Entering directory %a [$@] && \
-	pushd %a &&                                        \
-	$(MAKE) /NOLOGO $@ &&                              \
-	$(BUILD_MSG) [BUILD] Leaving directory %a [$@] &&  \
-	popd                                               \
-	)                                          
+	@set BLDERRFILE=$(PROJECT_SOURCE_ROOT)\release\_BLDERRORS 
+	@if exist %BLDERRFILE% del /q %BLDERRFILE% >NUL          
+	@for %a in ($**) do @(                                   \
+	$(BUILD_MSG) [BUILD] Entering directory %a [$@] &        \
+	pushd %a &                                               \
+	$(MAKE) /NOLOGO $@ &                                     \
+	call $(PROJECT_SOURCE_ROOT)\chkerr.cmd %a %ERRORLEVEL% & \
+	$(BUILD_MSG) [BUILD] Leaving directory %a [$@] &         \
+	popd                                                     \
+	)                                                        
+	@if exist %BLDERRFILE% (                                 \
+	$(BUILD_MSG) [BUILD] Build failed due to build errors. & \
+	$(BUILD_MSG) [BUILD] Failing projects were: &            \
+	$(BUILD_MSGFILE) %BLDERRFILE% &                          \
+	del /q %BLDERRFILE% >NUL &                               \
+	exit 1                                                   \
+	)                                                        
 
 clean: $(DIRS)
-	@for %a in ($**) do @(                             \
-	$(BUILD_MSG) [BUILD] Entering directory %a [$@] && \
-	pushd %a &&                                        \
-	$(MAKE) /NOLOGO $@ &&                              \
-	$(BUILD_MSG) [BUILD] Leaving directory %a [$@] &&  \
-	popd                                               \
-	)                                          
+	@set BLDERRFILE=$(PROJECT_SOURCE_ROOT)\release\_BLDERRORS 
+	@if exist %BLDERRFILE% del /q %BLDERRFILE% >NUL          
+	@for %a in ($**) do @(                                   \
+	$(BUILD_MSG) [BUILD] Entering directory %a [$@] &        \
+	pushd %a &                                               \
+	$(MAKE) /NOLOGO $@ &                                     \
+	call $(PROJECT_SOURCE_ROOT)\chkerr.cmd %a %ERRORLEVEL% & \
+	$(BUILD_MSG) [BUILD] Leaving directory %a [$@] &         \
+	popd                                                     \
+	)                                                        
+	@if exist %BLDERRFILE% (                                 \
+	$(BUILD_MSG) [BUILD] Build failed due to build errors. & \
+	$(BUILD_MSG) [BUILD] Failing projects were: &            \
+	$(BUILD_MSGFILE) %BLDERRFILE% &                          \
+	del /q %BLDERRFILE% >NUL &                               \
+	exit 1                                                   \
+	)                                                        
 
