d83091974c5255d8bfc18fc093eb4f485b9bfab1
Basilica <basilica@danifae.net>
2011-12-26 12:47:25 -0800

    Add better diagnostics for native script packager batch file.
    - Command log written to release\CLRBUILDLOG now.
    
    - Halt immediately if an intermediate step fails.

--- CLRScript/PackageScript.cmd
+++ CLRScript/PackageScript.cmd
@@ -1,6 +1,8 @@
 @echo off
 
-echo Packaging %1 into %2...
+echo Packaging %1 into %2 (from %CD%)... > ..\..\..\..\release\CLRBUILDLOG
 
-%~dp0\NativeScriptUtil.exe -i %1 -o %2 -q
-copy /y %2 ..\..\..\..\release
+%~dp0\NativeScriptUtil.exe -i %1 -o %2 -q >> ..\..\..\..\release\CLRBUILDLOG
+if %ERRORLEVEL% NEQ 0 ( goto :EOF )
+copy /y %2 ..\..\..\..\release >> ..\..\..\..\release\CLRBUILDLOG
+if %ERRORLEVEL% NEQ 0 ( goto :EOF )
