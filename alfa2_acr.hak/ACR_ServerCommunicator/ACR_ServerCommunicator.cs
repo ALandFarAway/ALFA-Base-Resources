41b1620bb69f4682cc968cd098d87079adf3c39b
Basilica <basilica@danifae.net>
2012-01-04 14:35:22 -0800

    Add skeleton for server communicator script.

---
+++ alfa2_acr.hak/ACR_ServerCommunicator/ACR_ServerCommunicator.cs
@@ -0,0 +1,47 @@
+﻿//
+// This script manages server-to-server IPC communication.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Reflection.Emit;
+using CLRScriptFramework;
+using ALFA;
+using NWScript;
+using NWScript.ManagedInterfaceLayer.NWScriptManagedInterface;
+
+using NWEffect = NWScript.NWScriptEngineStructure0;
+using NWEvent = NWScript.NWScriptEngineStructure1;
+using NWLocation = NWScript.NWScriptEngineStructure2;
+using NWTalent = NWScript.NWScriptEngineStructure3;
+using NWItemProperty = NWScript.NWScriptEngineStructure4;
+
+namespace ACR_ServerCommunicator
+{
+    public partial class ACR_ServerCommunicator : CLRScriptBase, IGeneratedScriptProgram
+    {
+
+        public ACR_ServerCommunicator([In] NWScriptJITIntrinsics Intrinsics, [In] INWScriptProgram Host)
+        {
+            InitScript(Intrinsics, Host);
+        }
+
+        private ACR_ServerCommunicator([In] ACR_ServerCommunicator Other)
+        {
+            InitScript(Other);
+
+            LoadScriptGlobals(Other.SaveScriptGlobals());
+        }
+
+        public static Type[] ScriptParameterTypes = { };
+
+        public Int32 ScriptMain([In] object[] ScriptParameters, [In] Int32 DefaultReturnCode)
+        {
+            return DefaultReturnCode;
+        }
+    }
+}
