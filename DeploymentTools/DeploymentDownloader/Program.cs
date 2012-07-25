207afdaf957e482f298e75f23ad05d741ecfece0
FoamBats4All <mystrasstalker@gmail.com>
2012-07-25 08:30:42 -0700

    Moved log deletion and prereq checking to the try block.

--- DeploymentTools/DeploymentDownloader/Program.cs
+++ DeploymentTools/DeploymentDownloader/Program.cs
@@ -12,21 +12,22 @@ namespace DeploymentDownloader
     {
         static void Main(string[] args)
         {
-            // Delete the old deployment logs.
-            if (File.Exists("DeploymentStager.log")) File.Delete("DeploymentStager.log");
-            if (File.Exists(SevenzipExtractor.LogFilename)) File.Delete(SevenzipExtractor.LogFilename);
-            if (File.Exists("DeploymentStager_Recompile.log")) File.Delete("DeploymentStager_Recompile.log");
-
-
-            // Make sure that we have the 7-zip extractor.
-            if (!File.Exists(SevenZipFilename)) throw new Exception(string.Format("{0} not found!", SevenZipFilename));
-
-            // Verify that we have the Advanced Script Compiler.
-            if (!File.Exists(ScriptCompilerFilename)) throw new Exception(string.Format("{0} not found!", ScriptCompilerFilename));
-
             ALFADeployerTool DeployerTool = new ALFADeployerTool();
             try
             {
+                // Delete the old deployment logs.
+                if (File.Exists("DeploymentStager.log")) File.Delete("DeploymentStager.log");
+                if (File.Exists(SevenzipExtractor.LogFilename)) File.Delete(SevenzipExtractor.LogFilename);
+                if (File.Exists("DeploymentStager_Recompile.log")) File.Delete("DeploymentStager_Recompile.log");
+
+
+                // Make sure that we have the 7-zip extractor.
+                if (!File.Exists(SevenZipFilename)) throw new Exception(string.Format("{0} not found!", SevenZipFilename));
+
+                // Verify that we have the Advanced Script Compiler.
+                if (!File.Exists(ScriptCompilerFilename)) throw new Exception(string.Format("{0} not found!", ScriptCompilerFilename));
+
+                // Run the deployment tool.
                 DeployerTool.Run();
             }
             catch (Exception e)
