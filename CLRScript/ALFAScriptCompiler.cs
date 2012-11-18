75bfc5fdf87b0b4089fb42599f2c5ffdaa18056a
Basilica <basilica@danifae.net>
2012-11-17 20:56:57 -0800

    Add ALFA.ScriptCompiler.CompileScript().
    - This function, which requires that NWNScriptCompiler.exe be placed
      in the NWNX4 installation directory, invokes the script compiler
      to compile a set of script source files.  It will be used by the
      content updater if a recompile was requested for a content hotfix.
    
    - Typical usage:
    
      ALFA.ScriptCompiler.CompilerResult Result;
    
      Result = ALFA.ScriptCompiler.CompileScript("*.nss", "-eov1.70");
    
      if (Result.Compiled)
      {
          // Succeeded, but warnings may be in Result.Warnings.
      }
      else
      {
          // Failed, errors in Result.Errors.
      }
    
    - The function assumes that scripts in the current module should be
      compiled, and that game data files should be loaded.
    
    - Optionally, the caller can supply a CompilerParseLineDelegate that
      is called back for each line that the compiler prints to stdout.
      This facility can be used to perform customized parsing, or to
      print each line (error or warning or informational) to a log file.

---
+++ CLRScript/ALFAScriptCompiler.cs
@@ -0,0 +1,187 @@
+//
+// This module contains logic for compiling scripts by invoking the script
+// compiler program.
+// 
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace ALFA
+{
+
+    /// <summary>
+    /// This class encapsulates general system information about the game
+    /// environment and configuration.
+    /// </summary>
+    public static class ScriptCompiler
+    {
+
+        /// <summary>
+        /// This structure describes the result of a compilation attempt.
+        /// </summary>
+        public struct CompilerResult
+        {
+            /// <summary>
+            /// True if the compilation succeeded.
+            /// </summary>
+            public bool Compiled;
+
+            /// <summary>
+            /// The list of warning messages.
+            /// </summary>
+            public IList<string> Warnings;
+
+            /// <summary>
+            /// The list of error messages.
+            /// </summary>
+            public IList<string> Errors;
+        };
+
+        /// <summary>
+        /// A function to be called on each line that the compiler prints to
+        /// standard output.
+        /// </summary>
+        /// <param name="Line">Supplies the line to parse.</param>
+        /// <returns>If true, the line should be ignored by the library and
+        /// not parsed.  Otherwise, if false, the library parses the line and
+        /// accumulates warning and error messages as appropriate.</returns>
+        public delegate bool CompilerParseLineDelegate(string Line);
+
+        /// <summary>
+        /// Compile a script (or set of scripts), and return information about
+        /// the compilation request.
+        /// </summary>
+        /// <param name="Filespec">Supplies a filespec of files to compile,
+        /// such as *.nss.</param>
+        /// <param name="Options">Optionally supplies additional compiler
+        /// options (e.g. -e -v1.70).</param>
+        /// <param name="ParseLine">Optionally supplies a function to parse
+        /// each line printed by the compiler.</param>
+        /// <returns>A compiler result descriptor is returned.</returns>
+        public static CompilerResult CompileScript(string Filespec, string Options, CompilerParseLineDelegate ParseLine = null)
+        {
+            CompilerResult Result = new CompilerResult();
+
+            Result.Compiled = false;
+            Result.Warnings = new List<string>();
+            Result.Errors = new List<string>();
+
+            try
+            {
+                //
+                // Construct the command line for the compiler instance.
+                //
+
+                string CmdLine = CreateCompilerCommandLine(Filespec, Options);
+                Process CompilerProcess;
+                ProcessStartInfo StartInfo = new ProcessStartInfo(GetCompilerExe(), CmdLine);
+                string Line;
+
+                //
+                // Start the compiler process and begin reading stdout until
+                // end of file is reached.  In the absence of any error
+                // messages observed, the operation is assumed to have
+                // completed successfully.
+                //
+
+                StartInfo.CreateNoWindow = true;
+                StartInfo.UseShellExecute = false;
+                StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                StartInfo.RedirectStandardOutput = true;
+
+                CompilerProcess = Process.Start(StartInfo);
+                Result.Compiled = true;
+
+                while ((Line = CompilerProcess.StandardOutput.ReadLine()) != null)
+                {
+                    //
+                    // Parse each line, examining it for a compiler diagnostic
+                    // indicator.  Accumulate errors and warnings into their
+                    // respective diagnostic message lists for the caller to
+                    // examine.
+                    //
+
+                    if (ParseLine != null && ParseLine(Line))
+                        continue;
+
+                    if (Line.StartsWith("Error:"))
+                    {
+                        Result.Errors.Add(Line);
+                        Result.Compiled = false;
+                    }
+                    else if (Line.Contains("): Error: NSC"))
+                    {
+                        Result.Errors.Add(Line);
+                        Result.Compiled = false;
+                    }
+                    else if (Line.StartsWith("Warning:"))
+                    {
+                        Result.Warnings.Add(Line);
+                    }
+                    else if (Line.Contains("): Warning: NSC"))
+                    {
+                        Result.Warnings.Add(Line);
+                    }
+                }
+
+                CompilerProcess.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                Result.Errors.Add(String.Format(
+                    "ALFA.ScriptCompiler.CompileScript({0}, {1}): Exception: '{2}'.",
+                    Filespec,
+                    Options,
+                    e));
+                Result.Compiled = false;
+            }
+
+            return Result;
+        }
+
+        /// <summary>
+        /// Get the path to NWNScriptCompiler.exe.
+        /// </summary>
+        /// <returns>The compiler EXE path.</returns>
+        private static string GetCompilerExe()
+        {
+            return ALFA.SystemInfo.GetNWNX4InstallationDirectory() + "NWNScriptCompiler.exe";
+        }
+
+        /// <summary>
+        /// Create the command line for the compiler given a filespec to
+        /// attempt to compile.
+        /// </summary>
+        /// <param name="Filespec">Supplies the file specification of the
+        /// target file(s) to compile.</param>
+        /// <param name="Options">Supplies additional compiler options.</param>
+        /// <returns>A compiler command line to compile the given script(s) is
+        /// returned.</returns>
+        private static string CreateCompilerCommandLine(string Filespec, string Options)
+        {
+            string InstallDir = ALFA.SystemInfo.GetGameInstallationDirectory();
+            string ModuleDir = ALFA.SystemInfo.GetModuleDirectory();
+
+            if (ModuleDir == null)
+                throw new ApplicationException("Unable to determine module directory; verify that NWNX4 was configured to start nwn2server with a -module option.");
+
+            if (Options == null)
+                Options = "";
+
+            return String.Format("-c -n {0} -r {1} {2} {3}",
+                InstallDir,
+                ModuleDir,
+                Options,
+                Filespec);
+        }
+
+    }   
+}
+
