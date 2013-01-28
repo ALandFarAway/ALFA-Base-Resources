d319b5117a18f0fe01221691ff821756331c1e1f
FoamBats4All <mystrasstalker@gmail.com>
2013-01-28 01:14:43 -0800

    Brought some NWN2 dependency DLLs into the dependency folder. Example/empty toolset plugin provided. Visual Studio 2010 solution added to manage any toolset plugins. ABR Creator references new DLL files.

---
+++ toolset_plugins/ACR_EmptyPlugin/PluginMain.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NWN2Toolset;
+using NWN2Toolset.Plugins;
+using TD.SandBar;
+
+namespace ACR_EmptyPlugin
+{
+    public class PluginMain : INWN2Plugin
+    {
+        private MenuButtonItem m_cMenuItem;
+
+        private void HandlePluginLaunch(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Load(INWN2PluginHost cHost)
+        {
+
+        }
+
+        public void Shutdown(INWN2PluginHost cHost)
+        {
+
+        }
+
+        public void Startup(INWN2PluginHost cHost)
+        {
+            m_cMenuItem = cHost.GetMenuForPlugin(this);
+            m_cMenuItem.Activate += new EventHandler(this.HandlePluginLaunch);
+        }
+
+        public void Unload(INWN2PluginHost cHost)
+        {
+
+        }
+
+        public MenuButtonItem PluginMenuItem
+        {
+            get
+            {
+                return m_cMenuItem;
+            }
+        }
+
+        public string DisplayName
+        {
+            get
+            {
+                return "Plugin Name";
+            }
+        }
+
+        public string MenuName
+        {
+            get
+            {
+                return "Plugin Name";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Plugin Name";
+            }
+        }
+
+
+        public object Preferences
+        {
+            get
+            {
+                return null;
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
