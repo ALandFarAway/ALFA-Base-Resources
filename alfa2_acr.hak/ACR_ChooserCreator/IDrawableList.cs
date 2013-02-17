e5db3a326f7ac313c0538b839c9c8b6fcfa99504
Zelknolf <zelknolf@yahoo.com>
2013-02-17 16:48:10 -0600

    Present interfaces for resources loaded in the background; present interfaces for lists that can be drawn in the creator.

---
+++ alfa2_acr.hak/ACR_ChooserCreator/IDrawableList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ALFA.Shared;
+
+namespace ACR_ChooserCreator
+{
+    interface IDrawableList : IBackgroundLoadedResource
+    {
+        bool ResourcesLoaded { get; set; }
+        List<Object> drawableList { get; set; }
+    }
+}
---
+++ alfa2_acr.hak/ALFA.Shared/IBackgroundLoadedResource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ALFA.Shared
+{
+    public interface IBackgroundLoadedResource
+    {
+        bool ResourcesLoaded { get; set; }
+    }
+}
