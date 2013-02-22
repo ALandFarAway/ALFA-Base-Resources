fc2644dd7664a4d85bdb4479bdea77a82aa872c4
Zelknolf <zelknolf@yahoo.com>
2013-02-21 22:34:28 -0600

    Add a user class, to track the users' current locations inside of the Navigators.

---
+++ alfa2_acr.hak/ACR_ChooserCreator/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ACR_ChooserCreator
+{
+    public class User
+    {
+        public uint Id;
+        public NavigatorCategory CurrentCreatureCategory;
+        public NavigatorCategory CurrentPlaceableCategory;
+        public NavigatorCategory CurrentItemCategory;
+    }
+}
