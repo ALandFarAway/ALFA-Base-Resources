970e755f064c690d00368078f64b283d4fad35fa
Zelknolf <Zelknolf@yahoo.com>
2014-02-13 18:26:44 -0600

    Create the ACR_Movement project

---
+++ alfa2_acr.hak/ACR_Movement/ACR_Movement.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Threading;
+using CLRScriptFramework;
+using ALFA;
+using NWScript;
+using NWScript.ManagedInterfaceLayer.NWScriptManagedInterface;
+
+using NWEffect = NWScript.NWScriptEngineStructure0;
+using NWEvent = NWScript.NWScriptEngineStructure1;
+using NWLocation = NWScript.NWScriptEngineStructure2;
+using NWTalent = NWScript.NWScriptEngineStructure3;
+using NWItemProperty = NWScript.NWScriptEngineStructure4;
+
+namespace ACR_Movement
+{
+    public partial class ACR_Movement : CLRScriptBase, IGeneratedScriptProgram
+    {
+        public ACR_Movement([In] NWScriptJITIntrinsics Intrinsics, [In] INWScriptProgram Host)
+        {
+            InitScript(Intrinsics, Host);
+        }
+
+        private ACR_Movement([In] ACR_Movement Other)
+        {
+            InitScript(Other);
+
+            LoadScriptGlobals(Other.SaveScriptGlobals());
+        }
+
+        public static Type[] ScriptParameterTypes =
+        { typeof(int) };
+
+        public Int32 ScriptMain([In] object[] ScriptParameters, [In] Int32 DefaultReturnCode)
+        {
+            int Value = (int)ScriptParameters[0]; // ScriptParameterTypes[0] is typeof(int)
+
+            PrintInteger(Value);
+
+            return 0;
+        }
+
+    }
+}
