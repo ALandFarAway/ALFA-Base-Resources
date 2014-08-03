136102355c6734f1dc40350007ff09e3be1d5a50
FoamBats4All <mystrasstalker@gmail.com>
2014-08-03 16:36:02 -0700

    Add some validation for items (price) and creatures (walk rate, perception, faction). Also cleaned up some unnecessary calls to String.Format, and hopefully made the progress bars more accurate.

--- toolset_plugins/ACR_BuilderPlugin/ValidateWindow.cs
+++ toolset_plugins/ACR_BuilderPlugin/ValidateWindow.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 
 using NWN2Toolset.NWN2.Data;
 using NWN2Toolset.NWN2.Data.Blueprints;
+using NWN2Toolset.NWN2.Data.Factions;
 using NWN2Toolset.NWN2.Data.Templates;
 using NWN2Toolset.NWN2.Data.TypedCollections;
 
@@ -40,80 +41,157 @@ namespace ACR_BuilderPlugin
             // Counters and commonly used variables.
             int i = 0;
             int count = 0;
+            int currentTask = 0;
+            int totalTasks = 3;
             NWN2BlueprintCollection bpCollection;
 
             // Open our log file.
             log = new System.IO.StreamWriter("acr_validation.log");
             log.WriteLine("ACR Validation Tool - Log");
 
+            #region Validate item blueprints.
+            log.WriteLine("\nValidating blueprints: Items");
+            pbOverall.Value = 100 * currentTask++ / totalTasks;
+            lbCurrentTask.Text = "Validating item blueprints...";
+            bpCollection = NWN2GlobalBlueprintManager.GetBlueprintsOfType(NWN2ObjectType.Item);
+            i = 0;
+            count = bpCollection.Count;
+            foreach (NWN2ItemBlueprint item in bpCollection)
+            {
+                // Report progress.
+                i++;
+                bwMain.ReportProgress(100 * i / count, "Validating \"" + item.ResourceName.ToString() + "\"");
+
+                try
+                {
+                    // Make sure the price isn't negative.
+                    if (item.Cost + item.AdditionalCost < 0)
+                    {
+                        log.WriteLine("ERROR: Item \"{0}\" has a negative item value.", item.ResourceName);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    log.WriteLine("EXCEPTION: Error while handling \"{0}\":\n{1}", item.ResourceName, exception.Message);
+                }
+            }
+            #endregion
+
+            #region Validate creature blueprints.
+            log.WriteLine("\nValidating blueprints: Creatures");
+            pbOverall.Value = 100 * currentTask++ / totalTasks;
+            lbCurrentTask.Text = "Validating creature blueprints...";
+            bpCollection = NWN2GlobalBlueprintManager.GetBlueprintsOfType(NWN2ObjectType.Creature);
+            i = 0;
+            count = bpCollection.Count;
+            foreach (NWN2CreatureBlueprint creature in bpCollection)
+            {
+                // Report progress.
+                i++;
+                bwMain.ReportProgress(100 * i / count, "Validating \"" + creature.ResourceName.ToString() + "\"");
+
+                try
+                {
+                    // Check for invalid factions.
+                    if (creature.FactionID == NWN2Faction.INVALID_FACTION_ID)
+                    {
+                        log.WriteLine("ERROR: Creature \"{0}\" has an invalid faction.", creature.ResourceName);
+                    }
+
+                    // Check for common perception range problems.
+                    if (creature.PerceptionRange.Row != 17 && creature.PerceptionRange.Row != 18)
+                    {
+                        log.WriteLine("WARNING: Creature \"{0}\" has a non-standard perception range. Use a suggested value of Player-5 or Player-10.", creature.ResourceName);
+                    }
+
+                    // Check for a walk rate equal to players.
+                    if (creature.WalkRate.Row == 0 || creature.WalkRate.Row == 19)
+                    {
+                        log.WriteLine("ERROR: Creature \"{0}\" has a walk rate equal to players. This will cause issues, and should be changed.", creature.ResourceName);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    log.WriteLine("EXCEPTION: Error while handling \"{0}\":\n{1}", creature.ResourceName, exception.Message);
+                }
+            }
+            #endregion
+
             #region Validate waypoint blueprints.
             log.WriteLine("\nValidating blueprints: Waypoints");
-            pbOverall.Value = 66;
+            pbOverall.Value = 100 * currentTask++ / totalTasks;
             lbCurrentTask.Text = "Validating waypoint blueprints...";
             bpCollection = NWN2GlobalBlueprintManager.GetBlueprintsOfType(NWN2ObjectType.Waypoint);
             i = 0;
             count = bpCollection.Count;
             foreach (NWN2WaypointBlueprint waypoint in bpCollection)
             {
-                // Check for common errors in the ACR_Spawn system.
-                if (waypoint.Variables.GetVariable("ACR_SPAWN_TYPE") != null)
-                {
-                    // Check variable types.
-                    ValidateVariableType(waypoint, "ACR_SPAWN_TYPE", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_RESNAMES_MIN", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_RESNAMES_MAX", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_RESPAWN_COUNT", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_RESPAWN_DELAY_MIN", NWN2ScriptVariableType.Float);
-                    ValidateVariableType(waypoint, "ACR_RESPAWN_DELAY_MAX", NWN2ScriptVariableType.Float);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_CHANCE", NWN2ScriptVariableType.Float);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_RANDOM_RADIUS", NWN2ScriptVariableType.Float);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_RANDOM_RANGE", NWN2ScriptVariableType.Float);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_IN_VFX", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_VFX", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_IN_SFX", NWN2ScriptVariableType.String);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_SFX", NWN2ScriptVariableType.String);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_ANIMATION", NWN2ScriptVariableType.String);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_IN_HOUR", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_OUT_HOUR", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_IN_DAY", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_OUT_DAY", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_IN_MONTH", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_OUT_MONTH", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_IN_PC_SIGHT", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_ONLY_WHEN_NO_PC_IN_AREA", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_WITH_ANIMATION", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_IS_DISABLED", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_RANDOM_FACING", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_IN_STEALTH", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_IN_DETECT", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_SPAWN_BUFFED", NWN2ScriptVariableType.Int);
-                    ValidateVariableType(waypoint, "ACR_COLOR_NAME", NWN2ScriptVariableType.String);
-
-                    // Check variable bounds.
-                    ValidateVariableBounds(waypoint, "ACR_SPAWN_TYPE", 0, 8);
-                    ValidateVariableBounds(waypoint, "ACR_SPAWN_RESNAMES_MIN", 0, 31);
-                    ValidateVariableBounds(waypoint, "ACR_SPAWN_RESNAMES_MAX", 0, 31);
-                    ValidateVariableBounds(waypoint, "ACR_SPAWN_CHANCE", 0.0f, 100.0f);
-                    if (!GetAreVariablesEqual(waypoint, "ACR_SPAWN_IN_HOUR", "ACR_SPAWN_OUT_HOUR"))
-                    {
-                        ValidateVariableBounds(waypoint, "ACR_SPAWN_IN_HOUR", 0, 23);
-                        ValidateVariableBounds(waypoint, "ACR_SPAWN_OUT_HOUR", 0, 23);
-                    }
-                    if (!GetAreVariablesEqual(waypoint, "ACR_SPAWN_IN_DAY", "ACR_SPAWN_OUT_DAY"))
-                    {
-                        ValidateVariableBounds(waypoint, "ACR_SPAWN_IN_DAY", 1, 31);
-                        ValidateVariableBounds(waypoint, "ACR_SPAWN_OUT_DAY", 1, 31);
-                    }
-                    if (!GetAreVariablesEqual(waypoint, "ACR_SPAWN_IN_MONTH", "ACR_SPAWN_OUT_MONTH"))
-                    {
-                        ValidateVariableBounds(waypoint, "ACR_SPAWN_IN_MONTH", 1, 12);
-                        ValidateVariableBounds(waypoint, "ACR_SPAWN_OUT_MONTH", 1, 12);
-                    }
-                }
-
                 // Report progress.
                 i++;
                 bwMain.ReportProgress(100 * i / count, "Validating \"" + waypoint.ResourceName.ToString() + "\"");
+                
+                try
+                {
+                    // Check for common errors in the ACR_Spawn system.
+                    if (waypoint.Variables.GetVariable("ACR_SPAWN_TYPE") != null)
+                    {
+                        // Check variable types.
+                        ValidateVariableType(waypoint, "ACR_SPAWN_TYPE", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_RESNAMES_MIN", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_RESNAMES_MAX", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_RESPAWN_COUNT", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_RESPAWN_DELAY_MIN", NWN2ScriptVariableType.Float);
+                        ValidateVariableType(waypoint, "ACR_RESPAWN_DELAY_MAX", NWN2ScriptVariableType.Float);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_CHANCE", NWN2ScriptVariableType.Float);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_RANDOM_RADIUS", NWN2ScriptVariableType.Float);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_RANDOM_RANGE", NWN2ScriptVariableType.Float);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_IN_VFX", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_VFX", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_IN_SFX", NWN2ScriptVariableType.String);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_SFX", NWN2ScriptVariableType.String);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_ANIMATION", NWN2ScriptVariableType.String);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_IN_HOUR", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_OUT_HOUR", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_IN_DAY", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_OUT_DAY", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_IN_MONTH", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_OUT_MONTH", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_IN_PC_SIGHT", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_ONLY_WHEN_NO_PC_IN_AREA", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_WITH_ANIMATION", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_IS_DISABLED", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_RANDOM_FACING", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_IN_STEALTH", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_IN_DETECT", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_SPAWN_BUFFED", NWN2ScriptVariableType.Int);
+                        ValidateVariableType(waypoint, "ACR_COLOR_NAME", NWN2ScriptVariableType.String);
+
+                        // Check variable bounds.
+                        ValidateVariableBounds(waypoint, "ACR_SPAWN_TYPE", 0, 8);
+                        ValidateVariableBounds(waypoint, "ACR_SPAWN_RESNAMES_MIN", 0, 31);
+                        ValidateVariableBounds(waypoint, "ACR_SPAWN_RESNAMES_MAX", 0, 31);
+                        ValidateVariableBounds(waypoint, "ACR_SPAWN_CHANCE", 0.0f, 100.0f);
+                        if (!GetAreVariablesEqual(waypoint, "ACR_SPAWN_IN_HOUR", "ACR_SPAWN_OUT_HOUR"))
+                        {
+                            ValidateVariableBounds(waypoint, "ACR_SPAWN_IN_HOUR", 0, 23);
+                            ValidateVariableBounds(waypoint, "ACR_SPAWN_OUT_HOUR", 0, 23);
+                        }
+                        if (!GetAreVariablesEqual(waypoint, "ACR_SPAWN_IN_DAY", "ACR_SPAWN_OUT_DAY"))
+                        {
+                            ValidateVariableBounds(waypoint, "ACR_SPAWN_IN_DAY", 1, 31);
+                            ValidateVariableBounds(waypoint, "ACR_SPAWN_OUT_DAY", 1, 31);
+                        }
+                        if (!GetAreVariablesEqual(waypoint, "ACR_SPAWN_IN_MONTH", "ACR_SPAWN_OUT_MONTH"))
+                        {
+                            ValidateVariableBounds(waypoint, "ACR_SPAWN_IN_MONTH", 1, 12);
+                            ValidateVariableBounds(waypoint, "ACR_SPAWN_OUT_MONTH", 1, 12);
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    log.WriteLine(String.Format("EXCEPTION: Error while handling \"{0}\":\n{1}", waypoint.ResourceName, exception.Message));
+                }
             }
             #endregion
 
@@ -153,11 +231,11 @@ namespace ACR_BuilderPlugin
                 if (enforce)
                 {
                     waypoint.Variables.GetVariable(variable).VariableType = type;
-                    log.WriteLine(String.Format("FIXED: Waypoint blueprint \"{0}\" has variable \"{1}\" not of type {2}.", waypoint.ResourceName, variable, type.ToString()));
+                    log.WriteLine("FIXED: Waypoint blueprint \"{0}\" has variable \"{1}\" not of type {2}.", waypoint.ResourceName, variable, type.ToString());
                 }
                 else
                 {
-                    log.WriteLine(String.Format("ERROR: Waypoint blueprint \"{0}\" has variable \"{1}\" not of type {2}.", waypoint.ResourceName, variable, type.ToString()));
+                    log.WriteLine("ERROR: Waypoint blueprint \"{0}\" has variable \"{1}\" not of type {2}.", waypoint.ResourceName, variable, type.ToString());
                 }
             }
         }
@@ -172,7 +250,7 @@ namespace ACR_BuilderPlugin
             if (waypoint.Variables.GetVariable(variable).VariableType != NWN2ScriptVariableType.Int) return;
             if (waypoint.Variables.GetVariable(variable).ValueInt < min || waypoint.Variables.GetVariable(variable).ValueInt > max)
             {
-                log.WriteLine(String.Format("ERROR: Waypoint blueprint \"{0}\" has variable \"{1}\" of value {2} outside of bounds [{3},{4}].", waypoint.ResourceName, variable, waypoint.Variables.GetVariable(variable).ValueInt, min, max));
+                log.WriteLine("ERROR: Waypoint blueprint \"{0}\" has variable \"{1}\" of value {2} outside of bounds [{3},{4}].", waypoint.ResourceName, variable, waypoint.Variables.GetVariable(variable).ValueInt, min, max);
             }
         }
         private void ValidateVariableBounds(NWN2WaypointBlueprint waypoint, string variable, float min, float max)
@@ -182,7 +260,7 @@ namespace ACR_BuilderPlugin
             if (waypoint.Variables.GetVariable(variable).VariableType != NWN2ScriptVariableType.Float) return;
             if (waypoint.Variables.GetVariable(variable).ValueFloat < min || waypoint.Variables.GetVariable(variable).ValueFloat > max)
             {
-                log.WriteLine(String.Format("ERROR: Waypoint blueprint \"{0}\" has variable \"{1}\" of value {2} outside of bounds [{3},{4}].", waypoint.ResourceName, variable, waypoint.Variables.GetVariable(variable).ValueFloat, min, max));
+                log.WriteLine("ERROR: Waypoint blueprint \"{0}\" has variable \"{1}\" of value {2} outside of bounds [{3},{4}].", waypoint.ResourceName, variable, waypoint.Variables.GetVariable(variable).ValueFloat, min, max);
             }
         }
 
