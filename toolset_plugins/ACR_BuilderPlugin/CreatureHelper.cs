bc22a99eea072d2ff203fe6f268449fab31b7cb1
FoamBats4All <mystrasstalker@gmail.com>
2014-08-06 13:11:37 -0700

    Add functionality to ACR_BuilderPlugin to globally adjust/fix creature hit points. A local variable can be set on creatures to force a hit point type (PnP, Maximum, Ignore). Handles undead and constructs.

---
+++ toolset_plugins/ACR_BuilderPlugin/CreatureHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NWN2Toolset.NWN2.Data.Templates;
+using NWN2Toolset.NWN2.Rules;
+
+namespace ACR_BuilderPlugin
+{
+    static class CreatureHelper
+    {
+        public enum HPType
+        {
+            Ignore = -1,
+            PenAndPaper = 1,
+            Maximum = 2
+        }
+
+        public static short GetHitPointsPNP(NWN2CreatureTemplate creature)
+        {
+            int maxHP = CalcMaxHitpointsFromHitDice(creature) / 2;
+
+            int newHP = (int)((float)maxHP / (float)creature.GetStatsCore().GetLevel() / 2);
+            newHP += creature.GetStatsCore().CalcHitPointModsFromFeats(1);
+            newHP += CalcHitPointModsFromStats(creature);
+            return (short)newHP;
+        }
+
+        public static short GetHitPointsMax(NWN2CreatureTemplate creature)
+        {
+            int newHP = CalcMaxHitpointsFromHitDice(creature);
+            newHP += creature.GetStatsCore().CalcHitPointModsFromFeats(1);
+            newHP += CalcHitPointModsFromStats(creature);
+            return (short)newHP;
+        }
+
+        public static int CalcMaxHitpointsFromHitDice(NWN2CreatureTemplate creature)
+        {
+            if (GetIsUndead(creature)) return creature.GetStatsCore().GetLevel() * 12;
+            return creature.GetStatsCore().CalcMaxHitpointsFromHitDice();
+        }
+
+        public static int CalcHitPointModsFromStats(NWN2CreatureTemplate creature)
+        {
+            if (GetIsUndead(creature)) return 0;
+            if (GetIsConstruct(creature)) return 0;
+            return creature.GetStatsCore().CalcHitPointModsFromStats();
+        }
+
+        public static bool GetIsConstruct(NWN2CreatureTemplate creature)
+        {
+            if (creature.Variables.GetVariable("ACR_CRE_ISCONSTRUCT").ValueInt == 1) return true;
+            if (creature.Variables.GetVariable("ACR_CRE_ISCONSTRUCT").ValueInt == 2) return false;
+            if (creature.Race.Row == 10) return true;
+            if (creature.GetStatsCore().GetHasClass(13) == 1) return true;
+            return false;
+        }
+
+        public static bool GetIsUndead(NWN2CreatureTemplate creature)
+        {
+            if (creature.Variables.GetVariable("ACR_CRE_ISUNDEAD").ValueInt == 1) return true;
+            if (creature.Variables.GetVariable("ACR_CRE_ISUNDEAD").ValueInt == 2) return false;
+            if (creature.Race.Row == 24) return true;
+            if (creature.GetStatsCore().GetHasClass(19) == 1) return true;
+            return false;
+        }
+    }
+}
--- toolset_plugins/ACR_BuilderPlugin/PluginMain.cs
+++ toolset_plugins/ACR_BuilderPlugin/PluginMain.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
-using NWN2Toolset;
-using NWN2Toolset.Plugins;
 using TD.SandBar;
 
+using NWN2Toolset;
 using NWN2Toolset.NWN2.Data;
 using NWN2Toolset.NWN2.Data.Blueprints;
+using NWN2Toolset.NWN2.Data.Instances;
 using NWN2Toolset.NWN2.Data.Templates;
 using NWN2Toolset.NWN2.Data.TypedCollections;
+using NWN2Toolset.Plugins;
 
 namespace ACR_BuilderPlugin
 {
@@ -29,6 +30,60 @@ namespace ACR_BuilderPlugin
             {
                 ModuleValidator validator = new ModuleValidator();
                 validator.Run();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
+            }
+        }
+
+        private void SetCreatureHPPnP(object sender, EventArgs e)
+        {
+            SetCreatureHP(CreatureHelper.HPType.PenAndPaper);
+        }
+
+        private void SetCreatureHPMax(object sender, EventArgs e)
+        {
+            SetCreatureHP(CreatureHelper.HPType.Maximum);
+        }
+
+        /// <summary>
+        /// Sets all creatures in the module to a certain HP.
+        /// </summary>
+        private void SetCreatureHP(CreatureHelper.HPType hpType)
+        {
+            if (MessageBox.Show("This tool will set creature hit points across the module. To blacklist a creature from this feature, add a local variable of type int with a name of ABP_SETHP_OVERRIDE and a value of -1. For further uses of ABP_SETHP_OVERRIDE, see the wiki.", "Set Creature Hit Points", MessageBoxButtons.OKCancel) == DialogResult.Cancel) return;
+            try
+            {
+                // First build our creature list.
+                List<NWN2CreatureTemplate> creatureList = new List<NWN2CreatureTemplate>();
+                foreach (NWN2CreatureBlueprint creature in NWN2ToolsetMainForm.App.Module.Creatures)
+                    creatureList.Add((NWN2CreatureTemplate)creature);
+                foreach (NWN2GameArea area in NWN2ToolsetMainForm.App.Module.Areas.Values)
+                    foreach (NWN2CreatureInstance creature in area.Creatures) 
+                        creatureList.Add((NWN2CreatureTemplate)creature);
+
+                // Go through them and fix HP.
+                foreach (NWN2CreatureTemplate creature in creatureList)
+                {
+                    // Check for blacklisting.
+                    if (creature.Variables.GetVariable("ABP_SETHP_OVERRIDE").ValueInt > 0) hpType = (CreatureHelper.HPType)creature.Variables.GetVariable("ABP_SETHP_OVERRIDE").ValueInt;
+                    if (hpType == CreatureHelper.HPType.Ignore) continue;
+
+                    // Find the new HP.
+                    short newHP = 0;
+                    switch (hpType)
+                    {
+                        case CreatureHelper.HPType.PenAndPaper: newHP = CreatureHelper.GetHitPointsPNP(creature); break;
+                        case CreatureHelper.HPType.Maximum: newHP = CreatureHelper.GetHitPointsMax(creature); break;
+                        default: continue;
+                    }
+
+                    // And set it.
+                    creature.BaseHitPoints = newHP;
+                    creature.CurrentHitPoints = newHP;
+                    creature.CharsheetHitPoints = newHP;
+                }
             }
             catch (Exception exception)
             {
@@ -66,6 +121,14 @@ namespace ACR_BuilderPlugin
         public void Startup(INWN2PluginHost cHost)
         {
             m_cMenuItem = cHost.GetMenuForPlugin(this);
+            int nCreaturesMenu = m_cMenuItem.Items.Add("Creatures");
+            {
+                int nSetHPMenu = m_cMenuItem.Items[nCreaturesMenu].Items.Add("Set Hit Points");
+                {
+                    m_cMenuItem.Items[nCreaturesMenu].Items[nSetHPMenu].Items.Add("Pen and Paper", SetCreatureHPPnP);
+                    m_cMenuItem.Items[nCreaturesMenu].Items[nSetHPMenu].Items.Add("Maximum", SetCreatureHPMax);
+                }
+            }
             m_cMenuItem.Items.Add("Validate Module", ValidateModule);
         }
 
